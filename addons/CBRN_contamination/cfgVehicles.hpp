b46e6f10e5430952381e2182118238bccfcdb9a8
kamkill01011 <kamil.amrani@heig-vd.ch>
2019-05-20 19:44:00 +0200

    respawn EH setVars on server

--- addons/CBRN_contamination/cfgVehicles.hpp
+++ addons/CBRN_contamination/cfgVehicles.hpp
@@ -6,7 +6,7 @@ class CfgVehicles {
 		{
 			class CBRN_Contamination
 			{
-				respawn = "(_this select 0) setVariable ['CBRN_contaminationLevel', 0];(_this select 0) setVariable ['CBRN_chemicalDamage', 0];(_this select 0) setVariable ['CBRN_chemicalDamageHandler', -1];";
+				respawn = "(_this select 0) setVariable ['CBRN_contaminationLevel', 0, 2];(_this select 0) setVariable ['CBRN_chemicalDamage', 0, 2];(_this select 0) setVariable ['CBRN_chemicalDamageHandler', -1, 2];";
 			};
 		};
 	};
