dbc665b20207028f1f43385d2addd8bc8d4f91e5
ASO-TheM <32593804+ASO-TheM@users.noreply.github.com>
2019-06-04 01:28:12 +0200

    Added 3 Inventory Sample Objects

--- addons/CBRN_gear/cfgVehicles.hpp
+++ addons/CBRN_gear/cfgVehicles.hpp
@@ -13,4 +13,48 @@ class CfgVehicles {
 			};
 		};
 	};
+	
+		 // Samples
+    class Item_Base_F;
+    class CBRN_Item_Sample_1: Item_Base_F {
+        author = "The_M";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Sample 1";
+        vehicleClass = "Items";
+		editorPreview = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg";
+        class TransportItems { 
+				class CBRN_Sample_1 {
+			    name = CBRN_Sample_1;
+                count = 1;  }         
+        };
+    };
+	   class CBRN_Item_Sample_2: Item_Base_F {
+        author = "The_M";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Sample 2";
+        vehicleClass = "Items";
+		editorPreview = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg";
+        class TransportItems { 
+				class CBRN_Sample_2 {
+			    name = CBRN_Sample_2;
+                count = 1;  }         
+        };
+    };
+	   class CBRN_Item_Sample_3: Item_Base_F {
+        author = "The_M";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Sample 3";
+        vehicleClass = "Items";
+		editorPreview = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg";
+        class TransportItems { 
+				class CBRN_Sample_3 {
+			    name = CBRN_Sample_3;
+                count = 1;  }         
+        };
+    };
+	
+	
 };
--- addons/CBRN_gear/cfgWeapons.hpp
+++ addons/CBRN_gear/cfgWeapons.hpp
@@ -44,6 +44,39 @@ class cfgWeapons
 		picture = "\CBRN_data\ui\items\NAAK.paa";
 		CBRN_disposeOnUse = 1;
 	};
+			
+    class CBRN_Sample_1: CBRN_itemBase {
+        author = "The_M";
+        scope = 2;
+		ace_arsenal_hide = 1
+        scopeCurator = 2;
+        displayName = "Sample 1"
+		model = "a3\Props_F_Orange\Humanitarian\Garbage\MedicalGarbage_01_Injector_F";
+		editorPreview = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg";
+		picture = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg"; 
+    };
+	
+	    class CBRN_Sample_2: CBRN_itemBase {
+        author = "The_M";
+        scope = 2;
+		ace_arsenal_hide = 1
+        scopeCurator = 2;
+        displayName = "Sample 2"
+		model = "a3\Props_F_Orange\Humanitarian\Garbage\MedicalGarbage_01_Injector_F";
+		editorPreview = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg";
+		picture = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg"; 
+    };
+	
+		    class CBRN_Sample_3: CBRN_itemBase {
+        author = "The_M";
+        scope = 2;
+		ace_arsenal_hide = 1
+        scopeCurator = 2;
+        displayName = "Sample 3"
+		model = "a3\Props_F_Orange\Humanitarian\Garbage\MedicalGarbage_01_Injector_F";
+		editorPreview = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg";
+		picture = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\MedicalGarbage_01_Injector_F.jpg"; 
+    };
 	
 	class GrenadeLauncher;
 	class Throw: GrenadeLauncher {
