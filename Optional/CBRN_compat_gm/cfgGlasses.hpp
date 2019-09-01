aa4e7aceed416cad683714f5b80b1e7493cec2cb
assaultboy <jromano269962@gmail.com>
2019-09-01 14:01:39 +0900

    Added GM Compatibility

---
+++ Optional/CBRN_compat_gm/cfgGlasses.hpp
@@ -0,0 +1,16 @@
+class cfgGlasses
+{
+	class gm_gasmask_base;
+	
+	class gm_ge_facewear_m65: gm_gasmask_base
+	{
+		CBRN_protectionLevel = 1 + 2;
+		CBRN_overlayType = "CBRN_compat_gm\data\hud\overlay_style_triangle.paa";
+	};
+	
+	class gm_gc_army_facewear_schm41m :gm_gasmask_base
+	{
+		CBRN_protectionLevel = 1 + 2;
+		CBRN_overlayType = "CBRN_gear\data\hud\overlay_style_round.paa";
+	};
+};
---
+++ Optional/CBRN_compat_gm/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches
+{
+	class CBRN_compat_gm
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"gm_core"};
+	};
+};
+
+#include "cfgGlasses.hpp"
